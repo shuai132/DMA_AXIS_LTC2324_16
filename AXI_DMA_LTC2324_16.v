c65aac9c352490d7ad21d5fdba8228c7fff0b816
liushuai <770722922@qq.com>
2019-11-16 20:27:37 +0800

    chore: refine code

--- AXI_DMA_LTC2324_16.v
+++ AXI_DMA_LTC2324_16.v
@@ -26,15 +26,16 @@ module AXI_DMA_LTC2324_16(
 );
 
 parameter       TEST_MODE   = 1'b0;
-   
-localparam      S_IDLE      = 0;
-localparam      S_SAMPLE    = 1;
-localparam      S_SAMP_WAIT = 2;
 
-reg[31:0]       sample_cnt;
-reg[7:0]        wait_cnt;
 reg[2:0]        state;
-   
+localparam      S_IDLE      = 3'd0;
+localparam      S_WAIT      = 3'd1;
+localparam      S_SAMP      = 3'd2;
+
+reg[4:0]        wait_cnt;
+localparam      wait_num    = 5'd20;
+reg[31:0]       sample_cnt;
+
 reg             fifo_wr_en;
 reg[15:0]       fifo_din;
 
@@ -61,8 +62,13 @@ reg             fifo_rd_en_d0;
 wire            fifo_empty;
 
 reg             adc_buf_en;
-reg[3:0]        write_cnt;
-localparam      write_num   = 4'd3;
+
+reg[1:0]        write_cnt;
+localparam      write_num   = 2'd3;
+localparam      write_cnt_0 = 2'd0;
+localparam      write_cnt_1 = 2'd1;
+localparam      write_cnt_2 = 2'd2;
+localparam      write_cnt_3 = 2'd3;
 
 wire            adc_data_valid;
 wire[15:0]      adc_ch1;
@@ -78,12 +84,12 @@ begin
         sample_start_d0 <= 1'b0;
         sample_start_d1 <= 1'b0;
         sample_start_d2 <= 1'b0;
-        sample_len_d0   <= 32'd0;
-        sample_len_d1   <= 32'd0;
-        sample_len_d2   <= 32'd0;
-        ch_sel_d0       <= 8'd0;
-        ch_sel_d1       <= 8'd0;
-        ch_sel_d2       <= 8'd0;
+        sample_len_d0   <= 1'b0;
+        sample_len_d1   <= 1'b0;
+        sample_len_d2   <= 1'b0;
+        ch_sel_d0       <= 1'b0;
+        ch_sel_d1       <= 1'b0;
+        ch_sel_d2       <= 1'b0;
     end
     else
     begin
@@ -103,9 +109,9 @@ always@(posedge adc_clk or posedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
     begin
-        state <= S_IDLE;
-        wait_cnt <= 8'd0;
-        sample_cnt <= 32'd0;
+        state       <= S_IDLE;
+        wait_cnt    <= 1'b0;
+        sample_cnt  <= 1'b0;
     end
     else
         case(state)
@@ -113,35 +119,35 @@ begin
             begin
               if (sample_start_d2)
               begin
-                state  <= S_SAMP_WAIT;
+                state  <= S_WAIT;
                 st_clr <= 1'b1;
               end
             end
-            S_SAMP_WAIT :
+            S_WAIT:
             begin
-              if(wait_cnt == 8'd20)
+              if(wait_cnt == wait_num)
               begin
-                state    <= S_SAMPLE;
-                wait_cnt <= 8'd0;
+                state    <= S_SAMP;
+                wait_cnt <= 1'b0;
               end
               else
               begin
-                  wait_cnt <= wait_cnt + 8'd1;
+                  wait_cnt <= wait_cnt + 1'b1;
               end
               st_clr <= 1'b0;
             end
-            S_SAMPLE:
+            S_SAMP:
             begin
               if (adc_data_valid)
               begin
                 if(sample_cnt == sample_len_d2 - 1)
                 begin
-                    sample_cnt <= 32'd0;
+                    sample_cnt <= 1'b0;
                     state <= S_IDLE;
                 end
                 else
                 begin
-                    sample_cnt <= sample_cnt + 32'd1;
+                    sample_cnt <= sample_cnt + 1'b1;
                 end
               end
             end
@@ -155,7 +161,7 @@ always@(posedge adc_clk or posedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
        adc_buf_en <= 1'b0;
-    else if (state == S_SAMPLE && adc_data_valid)
+    else if (state == S_SAMP && adc_data_valid)
        adc_buf_en <= 1'b1;
     else if (write_cnt == write_num)
        adc_buf_en <= 1'b0;
@@ -164,27 +170,27 @@ end
 always@(posedge adc_clk or posedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
-       write_cnt <= 4'd0;
+        write_cnt <= 1'b0;
     else if (adc_buf_en)
-       write_cnt <= write_cnt + 1'b1;
+        write_cnt <= write_cnt + 1'b1;
     else
-       write_cnt <= 4'd0;
+        write_cnt <= 1'b0;
 end
 
 always@(posedge adc_clk or posedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
     begin
-      fifo_din <= 16'd0;
+        fifo_din <= 1'b0;
     end
     else
     begin
       case(write_cnt)
-        4'd0   :  fifo_din <= adc_ch1;
-        4'd1   :  fifo_din <= adc_ch2;
-        4'd2   :  fifo_din <= adc_ch3;
-        4'd3   :  fifo_din <= adc_ch4;
-        default :  fifo_din <= 16'h5AA5;
+        write_cnt_0 :  fifo_din <= adc_ch1;
+        write_cnt_1 :  fifo_din <= adc_ch2;
+        write_cnt_2 :  fifo_din <= adc_ch3;
+        write_cnt_3 :  fifo_din <= adc_ch4;
+        default     :  fifo_din <= 16'h1234;
       endcase
     end
 end
@@ -192,9 +198,9 @@ end
 always@(posedge adc_clk or posedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
-       fifo_wr_en <= 1'b0;
+        fifo_wr_en <= 1'b0;
     else
-       fifo_wr_en <= adc_buf_en;
+        fifo_wr_en <= adc_buf_en;
 end
 
 afifo afifo_inst
@@ -228,7 +234,7 @@ LTC2324_16_inst
     .SDO3       (adc_SDO3),
     .SDO4       (adc_SDO4),
 
-    .sample_en  (state == S_SAMPLE),
+    .sample_en  (state == S_SAMP),
 
     .valid      (adc_data_valid),
     .ch1        (adc_ch1),
@@ -262,38 +268,38 @@ always@(posedge DMA_CLK or negedge DMA_RST_N)
 begin
     if(DMA_RST_N == 1'b0)
     begin
-        dma_len_d0  <= 32'd0;
-        dma_len_d1  <= 32'd0;
-        dma_len_d2  <= 32'd0;
+        dma_len_d0 <= 1'b0;
+        dma_len_d1 <= 1'b0;
+        dma_len_d2 <= 1'b0;
     end
     else
     begin
-         dma_len_d0 <= sample_len;
-         dma_len_d1 <= dma_len_d0;
-         dma_len_d2 <= dma_len_d1;
+        dma_len_d0 <= sample_len;
+        dma_len_d1 <= dma_len_d0;
+        dma_len_d2 <= dma_len_d1;
      end
 end
 
 always@(posedge DMA_CLK or negedge DMA_RST_N)
 begin
     if(DMA_RST_N == 1'b0)
-        dma_len <= 32'd0;
-    else if (fifo_rd_data_count > 10'd0)
+        dma_len <= 1'b0;
+    else if (fifo_rd_data_count > 1'b0)
         dma_len <= dma_len_d2;
 end
 
 always@(posedge DMA_CLK or negedge DMA_RST_N)
 begin
     if(DMA_RST_N == 1'b0)
-        dma_cnt <= 32'd0;
+        dma_cnt <= 1'b0;
     else if (DMA_AXIS_tvalid & ~DMA_AXIS_tlast)
         dma_cnt <= dma_cnt + 1'b1;
     else if (DMA_AXIS_tvalid & DMA_AXIS_tlast)
-        dma_cnt <= 32'd0;
+        dma_cnt <= 1'b0;
 end
 
 assign DMA_AXIS_tkeep  = 2'b11;
 assign DMA_AXIS_tvalid = DMA_AXIS_tready & (tvalid_en | fifo_rd_en_d0);
-assign DMA_AXIS_tlast  = DMA_AXIS_tvalid & (dma_cnt == (dma_len << 2) - 1);
+assign DMA_AXIS_tlast  = DMA_AXIS_tvalid & (dma_cnt == (dma_len << 2'd2) - 1'b1);
 
 endmodule
