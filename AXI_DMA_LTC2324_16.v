2b0c93431cf37458538ab0cd9b67d8ce596cd0a3
liushuai <770722922@qq.com>
2019-11-16 18:18:54 +0800

    feature: update logic

--- AXI_DMA_LTC2324_16.v
+++ AXI_DMA_LTC2324_16.v
@@ -11,14 +11,13 @@ module AXI_DMA_LTC2324_16(
     input               adc_SDO3,
     input               adc_SDO4,
 
-    // CTRL
     input[31:0]         sample_len,
     input               sample_start,
     output reg          st_clr,
+    input[7:0]          ch_sel,
 
-    // AXIS
-    output[7:0]         DMA_AXIS_tdata,
-    output[0:0]         DMA_AXIS_tkeep,
+    output[15:0]        DMA_AXIS_tdata,
+    output[1:0]         DMA_AXIS_tkeep,
     output              DMA_AXIS_tlast,
     input               DMA_AXIS_tready,
     output              DMA_AXIS_tvalid,
@@ -26,103 +25,193 @@ module AXI_DMA_LTC2324_16(
     input               DMA_CLK
 );
 
-parameter           TEST_MODE   = 1'b0;
+parameter       TEST_MODE   = 1'b0;
+   
+localparam      S_IDLE      = 0;
+localparam      S_SAMPLE    = 1;
+localparam      S_SAMP_WAIT = 2;
 
-reg[31:0]           sample_cnt;
-reg[31:0]           wait_cnt;
-reg                 fifo_wr_en;
-reg[7:0]            fifo_din;
-wire[9:0]           fifo_rd_data_count;
-wire                fifo_rd_en;
-reg                 fifo_rd_en_d0;
-wire                fifo_empty;
+reg[31:0]       sample_cnt;
+reg[7:0]        wait_cnt;
+reg[2:0]        state;
+   
+reg             fifo_wr_en;
+reg[15:0]       fifo_din;
 
-reg[31:0]           dma_cnt;
+reg             sample_start_d0;
+reg             sample_start_d1;
+reg             sample_start_d2;
+reg[31:0]       sample_len_d0;
+reg[31:0]       sample_len_d1;
+reg[31:0]       sample_len_d2;
+reg[7:0]        ch_sel_d0;
+reg[7:0]        ch_sel_d1;
+reg[7:0]        ch_sel_d2;
+                
+reg[31:0]       dma_len_d0;
+reg[31:0]       dma_len_d1;
+reg[31:0]       dma_len_d2;
+reg[31:0]       dma_len;
+reg[31:0]       dma_cnt;
 
-reg                 tvalid_en;
+reg             tvalid_en;
+wire[9:0]       fifo_rd_data_count;
+wire            fifo_rd_en;
+reg             fifo_rd_en_d0;
+wire            fifo_empty;
 
-reg[2:0]            state;
-localparam          S_IDLE      = 0;
-localparam          S_SAMPLE    = 1;
+reg             adc_buf_en;
+reg[3:0]        write_cnt;
+localparam      write_num   = 4'd3;
 
-// 这个axis ip核的数据位宽是8位，但是我们传输一次4个通道ADC数据每个16位，总共64位。分8次传送。
-reg[2+3:0]          write_cnt;
-localparam          write_num   = 4'd8;
+wire            adc_data_valid;
+wire[15:0]      adc_ch1;
+wire[15:0]      adc_ch2;
+wire[15:0]      adc_ch3;
+wire[15:0]      adc_ch4;
 
-// ADC
-wire                adc_valid;
-wire[15:0]          adc_ch1;
-wire[15:0]          adc_ch2;
-wire[15:0]          adc_ch3;
-wire[15:0]          adc_ch4;
-wire[63:0]          adc_data;
-assign              adc_data = {adc_ch4, adc_ch3, adc_ch2, adc_ch1};
-
-reg                 adc_valid_flag;
 
 always@(posedge adc_clk or negedge adc_rst_n)
 begin
     if(adc_rst_n == 1'b0)
     begin
-        state      <= S_IDLE;
-        wait_cnt   <= 32'd0;
-        sample_cnt <= 32'd0;
-        fifo_din   <= 8'd0;
-        fifo_wr_en <= 1'b0;
-        st_clr     <= 1'b0;
-        write_cnt  <= 1'b0;
-        adc_valid_flag <= 1'b0;
+        sample_start_d0 <= 1'b0;
+        sample_start_d1 <= 1'b0;
+        sample_start_d2 <= 1'b0;
+        sample_len_d0   <= 32'd0;
+        sample_len_d1   <= 32'd0;
+        sample_len_d2   <= 32'd0;
+        ch_sel_d0       <= 8'd0;
+        ch_sel_d1       <= 8'd0;
+        ch_sel_d2       <= 8'd0;
     end
     else
-    case(state)
-        S_IDLE:
-        begin
-            if (sample_start)
-            begin
-                st_clr <= 1'b1;
-                state  <= S_SAMPLE;
-            end
-        end
-        S_SAMPLE:
-        begin
-            if(sample_cnt == sample_len)
-            begin
-                sample_cnt <= 32'd0;
-                fifo_wr_en <= 1'b0;
-                state      <= S_IDLE;
-                st_clr     <= 1'b0;
-                fifo_din   <= 1'b0;
-                write_cnt  <= 1'b0;
-                adc_valid_flag <= 1'b0;
-            end
-            else
-            begin
-                if (adc_valid)
-                    adc_valid_flag <= 1'b1;
-
-                // adc_valid_flag用于把valid信号延长至读取完8个字节
-                if (adc_valid || adc_valid_flag) begin
-                    fifo_wr_en <= 1'b1;
-
-                    // arm是小端模式 这里也把高位字节放到前面 方便解析
-                    fifo_din <= adc_data >> (write_cnt << 3);
-
-                    if (write_cnt == write_num)
-                    begin
-                        write_cnt  <= 1'b0;
-                        fifo_wr_en <= 1'b0;
-                        sample_cnt <= sample_cnt + 32'd1;
-                        adc_valid_flag <= 1'b0;
-                    end
-                    else
-                        write_cnt  <= write_cnt + 1'b1;
-                end
-            end
-        end
-        default:
-            state <= S_IDLE;
-    endcase
+    begin
+         sample_start_d0 <= sample_start;
+         sample_start_d1 <= sample_start_d0;
+         sample_start_d2 <= sample_start_d1;
+         sample_len_d0   <= sample_len;
+         sample_len_d1   <= sample_len_d0;
+         sample_len_d2   <= sample_len_d1;
+         ch_sel_d0       <= ch_sel;
+         ch_sel_d1       <= ch_sel_d0;
+         ch_sel_d2       <= ch_sel_d1;
+     end
 end
+
+always@(posedge adc_clk or posedge adc_rst_n)
+begin
+    if(adc_rst_n == 1'b0)
+    begin
+        state <= S_IDLE;
+        wait_cnt <= 8'd0;
+        sample_cnt <= 32'd0;
+    end
+    else
+        case(state)
+            S_IDLE:
+            begin
+              if (sample_start_d2)
+              begin
+                state  <= S_SAMP_WAIT;
+                st_clr <= 1'b1;
+              end
+            end
+            S_SAMP_WAIT :
+            begin
+              if(wait_cnt == 8'd20)
+              begin
+                state    <= S_SAMPLE;
+                wait_cnt <= 8'd0;
+              end
+              else
+              begin
+                  wait_cnt <= wait_cnt + 8'd1;
+              end
+              st_clr <= 1'b0;
+            end
+            S_SAMPLE:
+            begin
+              if (adc_data_valid)
+              begin
+                if(sample_cnt == sample_len_d2 - 1)
+                begin
+                    sample_cnt <= 32'd0;
+                    state <= S_IDLE;
+                end
+                else
+                begin
+                    sample_cnt <= sample_cnt + 32'd1;
+                end
+              end
+            end
+            default:
+                state <= S_IDLE;
+        endcase
+end
+
+
+always@(posedge adc_clk or posedge adc_rst_n)
+begin
+    if(adc_rst_n == 1'b0)
+       adc_buf_en <= 1'b0;
+    else if (state == S_SAMPLE && adc_data_valid)
+       adc_buf_en <= 1'b1;
+    else if (write_cnt == write_num)
+       adc_buf_en <= 1'b0;
+end
+
+always@(posedge adc_clk or posedge adc_rst_n)
+begin
+    if(adc_rst_n == 1'b0)
+       write_cnt <= 4'd0;
+    else if (adc_buf_en)
+       write_cnt <= write_cnt + 1'b1;
+    else
+       write_cnt <= 4'd0;
+end
+
+always@(posedge adc_clk or posedge adc_rst_n)
+begin
+    if(adc_rst_n == 1'b0)
+    begin
+      fifo_din <= 16'd0;
+    end
+    else
+    begin
+      case(write_cnt)
+        4'd0   :  fifo_din <= adc_ch1;
+        4'd1   :  fifo_din <= adc_ch2;
+        4'd2   :  fifo_din <= adc_ch3;
+        4'd3   :  fifo_din <= adc_ch4;
+        default :  fifo_din <= 16'h5AA5;
+      endcase
+    end
+end
+
+always@(posedge adc_clk or posedge adc_rst_n)
+begin
+    if(adc_rst_n == 1'b0)
+       fifo_wr_en <= 1'b0;
+    else
+       fifo_wr_en <= adc_buf_en;
+end
+
+afifo afifo_inst
+(
+  .rst              (~DMA_RST_N     ),
+  .wr_clk           (adc_clk        ),
+  .rd_clk           (DMA_CLK        ),
+  .din              (fifo_din       ),
+  .wr_en            (fifo_wr_en     ),
+  .rd_en            (fifo_rd_en     ),
+  .dout             (DMA_AXIS_tdata ),
+  .full             (               ),
+  .empty            (fifo_empty     ),
+  .rd_data_count    (fifo_rd_data_count),
+  .wr_data_count    (               )
+);
+
 
 LTC2324_16
 #(.USE_SCK_SHIFT_DATA(TEST_MODE))
@@ -139,29 +228,15 @@ LTC2324_16_inst
     .SDO3       (adc_SDO3),
     .SDO4       (adc_SDO4),
 
-    .sample_en  ((state == S_SAMPLE) && (sample_cnt != sample_len)),
+    .sample_en  (state == S_SAMPLE),
 
-    .valid      (adc_valid),
+    .valid      (adc_data_valid),
     .ch1        (adc_ch1),
     .ch2        (adc_ch2),
     .ch3        (adc_ch3),
     .ch4        (adc_ch4)
 );
 
-afifo afifo_inst
-(
-    .rst              (~DMA_RST_N     ),
-    .wr_clk           (adc_clk        ),
-    .rd_clk           (DMA_CLK        ),
-    .din              (fifo_din       ),
-    .wr_en            (fifo_wr_en     ),
-    .rd_en            (fifo_rd_en     ),
-    .dout             (DMA_AXIS_tdata ),
-    .full             (               ),
-    .empty            (fifo_empty     ),
-    .rd_data_count    (fifo_rd_data_count),
-    .wr_data_count    (               )
-);
 
 assign fifo_rd_en = DMA_AXIS_tready && ~fifo_empty;
 
@@ -186,6 +261,30 @@ end
 always@(posedge DMA_CLK or negedge DMA_RST_N)
 begin
     if(DMA_RST_N == 1'b0)
+    begin
+        dma_len_d0  <= 32'd0;
+        dma_len_d1  <= 32'd0;
+        dma_len_d2  <= 32'd0;
+    end
+    else
+    begin
+         dma_len_d0 <= sample_len;
+         dma_len_d1 <= dma_len_d0;
+         dma_len_d2 <= dma_len_d1;
+     end
+end
+
+always@(posedge DMA_CLK or negedge DMA_RST_N)
+begin
+    if(DMA_RST_N == 1'b0)
+        dma_len <= 32'd0;
+    else if (fifo_rd_data_count > 10'd0)
+        dma_len <= dma_len_d2;
+end
+
+always@(posedge DMA_CLK or negedge DMA_RST_N)
+begin
+    if(DMA_RST_N == 1'b0)
         dma_cnt <= 32'd0;
     else if (DMA_AXIS_tvalid & ~DMA_AXIS_tlast)
         dma_cnt <= dma_cnt + 1'b1;
@@ -193,8 +292,8 @@ begin
         dma_cnt <= 32'd0;
 end
 
-assign DMA_AXIS_tkeep  = 1'b1;
+assign DMA_AXIS_tkeep  = 2'b11;
 assign DMA_AXIS_tvalid = DMA_AXIS_tready & (tvalid_en | fifo_rd_en_d0);
-assign DMA_AXIS_tlast  = DMA_AXIS_tvalid & (dma_cnt == sample_len - 1);
+assign DMA_AXIS_tlast  = DMA_AXIS_tvalid & (dma_cnt == (dma_len << 2) - 1);
 
 endmodule
